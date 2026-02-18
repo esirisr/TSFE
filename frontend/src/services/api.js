b977851242d02a770d1466a7d70bbccce0e4cedd
esirisr <esirisr123@gmail.com>
2026-02-18 19:27:21 +0300

    hh

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://tsbe-production.up.railway.app',
+  baseURL: 'https://tsbe-production.up.railway.app/api',
 });
 
 // Request Interceptor: Attach token to every outgoing request
